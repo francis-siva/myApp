d590bb4a810cff9fe0867755147d447bc7c68095
unknown <fsivasithamparam@gmail.com>
2018-11-16 01:10:37 +0100

    Ajout du package eslint & tests des Props ES6 sur React

--- App.js
+++ App.js
@@ -7,15 +7,31 @@
  */
 
 import React, {Component} from 'react';
-import {Text, View, Image } from 'react-native';
+import {AppRegistry, Text, View, Image } from 'react-native';
 
-export default class App extends Component {
+class App extends Component {
   render() {
+    let amgEType = require("./Classe_E_AMG.jpg");
     return (
-      <View>
+      <View style={{alignItems: 'center'}}>
         <Text>Hope it will work !!!</Text>
-        <Image source={require("./Classe_E_AMG.jpg")} style={{ width: 320, height: 160 }} />
+        <Image source={amgEType} style={{ width: 320, height: 160 }} />
+        <Text>Car Model {this.props.name}</Text>
       </View>
     );
   }
 }
+
+export default class LotsOfModel extends Component {
+  render() {
+    return (
+      <View style={{alignItems: 'center'}}>
+        <App name="Classe E" />
+        <App name="C63" />
+        <App name="SLR" />
+      </View>
+    );
+  }
+}
+
+AppRegistry.registerComponent('myApp', () => LotsOfModel);
